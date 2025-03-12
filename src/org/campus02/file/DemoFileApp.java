9adf68e6a64db870ea6a63ff0d779be9385d636e
stephan.kochauf@gmail.com <stephan.kochauf@gmail.com>
2025-03-12 16:26:56 +0100

    FileDemo

---
+++ src/org/campus02/file/DemoFileApp.java
@@ -0,0 +1,25 @@
+package org.campus02.file;
+
+import java.io.File;
+
+public class DemoFileApp {
+
+    public static void main(String[] args) {
+
+        //D:\temp\ETS25
+        File f = new File("D:\\temp\\ETS25\\test.txt");
+
+        System.out.println("f.length() = " + f.length());
+
+        File dir = new File("D:\\temp\\ETS25\\");
+        System.out.println("dir.length() = " + dir.length());
+
+        for (File fd : dir.listFiles()) {
+            System.out.println("f. = " + fd.getName() + ": " + fd.length());
+        }
+        
+        
+
+
+    }
+}
